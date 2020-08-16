9c20b708dee254bc981678a9e96a561d1c14d7c1
Demipo <41957499+Demipo@users.noreply.github.com>
2020-08-16 13:45:48 +0100

    modify security configuration

--- binchecker/src/main/java/com/example/binchecker/configuration/SecurityConfig.java
+++ binchecker/src/main/java/com/example/binchecker/configuration/SecurityConfig.java
@@ -1,28 +1,27 @@
 package com.example.binchecker.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("password")
-                .roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/admin/bins-details").hasRole("ADMIN")
+                .authorizeRequests().antMatchers("/bin/admin/bin-details").hasRole("ADMIN")
                 .anyRequest().permitAll()
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
 }
